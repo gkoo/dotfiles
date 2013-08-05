19928981a294ca4930e55a47b257613508f84eda
Gordon Koo <gordon@gordonkoo.com>
2013-08-05 12:07:37 -0700

    added slate.js

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ Just some dot files. Check out this directory to ~/.dotfiles and then:
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/gitignore ~/.gitignore
 ln -s ~/.dotfiles/git_branch_prompt.bash ~/.git_branch_prompt.bash
+ln -s ~/.dotfiles/slate ~/.slate
+ln -s ~/.dotfiles/slate.js ~/.slate.js
 ```
---
+++ slate.js
@@ -0,0 +1,65 @@
+// Create Operations
+var pushRight = slate.operation("push", {
+  "direction" : "right",
+  "style" : "bar-resize:screenSizeX/2"
+}),
+
+pushLeft = slate.operation("push", {
+  "direction" : "left",
+  "style" : "bar-resize:screenSizeX/2"
+});
+
+halfThrow0 = slate.operation("throw", {
+  "screen": "0",
+  "width" : "screenSizeX/2",
+  "height" : "screenSizeY"
+}),
+
+halfThrow1 = slate.operation("throw", {
+  "screen": "1",
+  "width" : "screenSizeX/2",
+  "height" : "screenSizeY"
+}),
+
+throw0 = slate.operation("throw", {
+  "screen": "0",
+  "width" : "screenSizeX",
+  "height" : "screenSizeY"
+}),
+
+throw1 = slate.operation("throw", {
+  "screen": "1",
+  "width" : "screenSizeX",
+  "height" : "screenSizeY"
+}),
+
+fullscreen = slate.operation("move", {
+  "x" : "screenOriginX",
+  "y" : "screenOriginY",
+  "width" : "screenSizeX",
+  "height" : "screenSizeY"
+});
+
+// Two-monitor set-up
+slate.bind("1:ctrl;cmd", function(win) {
+  win.doOperation(halfThrow0);
+  win.doOperation(pushLeft);
+});
+slate.bind("2:ctrl;cmd", function(win) {
+  win.doOperation(halfThrow0);
+  win.doOperation(pushRight);
+});
+slate.bind("3:ctrl;cmd", function(win) {
+  win.doOperation(halfThrow1);
+  win.doOperation(pushLeft);
+});
+slate.bind("4:ctrl;cmd", function(win) {
+  win.doOperation(halfThrow1);
+  win.doOperation(pushRight);
+});
+slate.bind("left:ctrl;cmd", function(win) {
+  win.doOperation(throw0);
+});
+slate.bind("right:ctrl;cmd", function(win) {
+  win.doOperation(throw1);
+});
