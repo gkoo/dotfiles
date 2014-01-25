9785676746b62d85a48fd12c224108b29c3e64ef
Gordon Koo <gordon.koo@airbedandbreakfast.com>
2014-01-25 09:52:40 -0800

    [Slate] Decompose slate config

--- slate.js
+++ slate.js
@@ -1,45 +1,27 @@
 /* globals slate */
 
-var pushRight = slate.operation('push', {
-  'direction' : 'right',
-  'style' : 'bar-resize:screenSizeX/2'
-}),
+// Push the window against an edge of the screen.
+// @direction: the direction to push
+// @width: desired window width represented as a fraction.
+var push = function(direction, width) {
+  var config = { direction: direction };
 
-pushLeft = slate.operation('push', {
-  'direction' : 'left',
-  'style' : 'bar-resize:screenSizeX/2'
-}),
+  config.style = 'bar-resize:screenSizeX*' + width;
 
-halfThrow0 = slate.operation('throw', {
-  'screen': '0',
-  'width' : 'screenSizeX/2',
-  'height' : 'screenSizeY'
-}),
+  return slate.operation('push', config);
+},
 
-halfThrow1 = slate.operation('throw', {
-  'screen': '1',
-  'width' : 'screenSizeX/2',
-  'height' : 'screenSizeY'
-}),
+// Move the window to a desired screen.
+// @screen: screen to move to. Defaults to screen 0.
+// @width: desired window width represented as a fraction. Defaults to 1 (full-screen).
+throww = function(screen, width) {
+  var config = { screen: screen || '0' };
 
-throw0 = slate.operation('throw', {
-  'screen': '0',
-  'width' : 'screenSizeX',
-  'height' : 'screenSizeY'
-}),
+  width = width || '1';
+  config.width = 'screenSizeX*' + width;
 
-throw1 = slate.operation('throw', {
-  'screen': '1',
-  'width' : 'screenSizeX',
-  'height' : 'screenSizeY'
-}),
-
-fullscreen = slate.operation('move', {
-  'x' : 'screenOriginX',
-  'y' : 'screenOriginY',
-  'width' : 'screenSizeX',
-  'height' : 'screenSizeY'
-}),
+  return slate.operation('throw', config);
+},
 
 adiumPush = slate.operation('push', {
   'direction': 'right'
@@ -56,27 +38,56 @@ appOperations = function(win) {
   return false;
 };
 
-// Two-monitor set-up
 slate.bind('1:ctrl;cmd', function(win) {
-  win.doOperation(halfThrow0);
-  win.doOperation(pushLeft);
+  if (slate.screenCount > 1) {
+    win.doOperation(throww('0', '1/2'));
+  }
+  win.doOperation(push('left', '1/2'));
 });
+
 slate.bind('2:ctrl;cmd', function(win) {
-  win.doOperation(halfThrow0);
-  win.doOperation(pushRight);
+  if (slate.screenCount > 1) {
+    win.doOperation(throww('0', '1/2'));
+  }
+  win.doOperation(push('right', '1/2'));
 });
+
 slate.bind('3:ctrl;cmd', function(win) {
-  win.doOperation(halfThrow1);
-  win.doOperation(pushLeft);
+  win.doOperation(throww('1', '1/2'));
+  win.doOperation(push('left', '1/2'));
 });
+
 slate.bind('4:ctrl;cmd', function(win) {
-  win.doOperation(halfThrow1);
-  win.doOperation(pushRight);
+  win.doOperation(throww('1', '1/2'));
+  win.doOperation(push('right', '1/2'));
 });
+
 slate.bind('left:ctrl;cmd', function(win) {
-  win.doOperation(throw0);
+  if (slate.screenCount > 1) {
+    // Two-screen setup
+    win.doOperation(throww('0'));
+  }
+  else {
+    // Single-screen setup
+    win.doOperation(push('left', '2/3'));
+  }
 });
+
 slate.bind('right:ctrl;cmd', function(win) {
-  if (appOperations(win)) { return; }
-  win.doOperation(throw1);
+  if (appOperations(win)) {
+    return;
+  }
+  else if (slate.screenCount > 1) {
+    // Two-screen setup
+    win.doOperation(throww('1'));
+  }
+  else {
+    // Single-screen setup
+    win.doOperation(push('right', '2/3'));
+  }
+});
+
+slate.bind('up:ctrl;cmd', function(win) {
+  // Only used for single-screen.
+  win.doOperation(throww('0'));
 });
